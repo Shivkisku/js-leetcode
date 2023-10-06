7ed03ff233ae770cd7444df8a6b163816efc72a7
Shivnath Kisku <124551959+Shivkisku@users.noreply.github.com>
2023-10-06 21:44:15 +0530

    Add files via upload

---
+++ 104.js
@@ -0,0 +1,14 @@
+const removeDuplicates = function(nums) {
+    for(let i = 0; i < nums.length;) {
+      if(i >= 2 && nums[i] === nums[i - 1] && nums[i] === nums[i - 2]) {
+         nums.splice(i, 1);
+      } else {
+        i++;
+      }
+    }
+    console.log(nums);
+};
+
+// Test the function
+const inputArray = [1, 1, 1, 2, 2, 3];
+removeDuplicates(inputArray);
