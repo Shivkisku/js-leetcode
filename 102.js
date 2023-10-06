1660161fabd3692ce99ce839eebd93ffe59be65e
Shivnath Kisku <124551959+Shivkisku@users.noreply.github.com>
2023-10-06 21:40:52 +0530

    Add files via upload

---
+++ 102.js
@@ -0,0 +1,23 @@
+function subsets(nums) {
+    const list = [];
+    const len = nums.length;
+    const subsetNum = Math.pow(2, len);
+    
+    for (let n = 0; n < subsetNum; n++) {
+      list[n] = [];
+    }
+    
+    for (let i = 0; i < len; i++) {
+      for (let j = 0; j < subsetNum; j++) {
+        if ((j >> i) & 1) {
+          list[j].push(nums[i]);
+        }
+      }
+    }
+    
+    return list;
+  }
+  
+  const result = subsets([1, 2, 3]);
+  console.log(result);
+  
